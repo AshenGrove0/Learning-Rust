2689f724e4dfd89434628fbd1f76743a912e9ea1
AshenGrove0 <108214445+AshenGrove0@users.noreply.github.com>
2024-05-13 19:58:04 +0100

    1.2

---
+++ the-book/hello_cargo/target/debug/deps/hello_cargo-07396f70e7ceaaaf.d
@@ -0,0 +1,5 @@
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/hello_cargo/target/debug/deps/hello_cargo-07396f70e7ceaaaf: src/main.rs
+
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/hello_cargo/target/debug/deps/hello_cargo-07396f70e7ceaaaf.d: src/main.rs
+
+src/main.rs:
---
+++ the-book/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/hello_cargo/target/debug/hello_cargo: /Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/hello_cargo/src/main.rs
