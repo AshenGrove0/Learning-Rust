067d17f688a5e8b798cddc7a94a418849638c944
AshenGrove0 <108214445+AshenGrove0@users.noreply.github.com>
2024-05-13 22:37:31 +0100

    3.1 - Variables

---
+++ the-book/variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+    const HOUR_IN_SECONDS: u32 = 60*60;
+    println!("24 hours in seconds is {}",HOUR_IN_SECONDS*24);
+    let y = 5;
+    println!("y is {y}");
+    let y = y + 1;
+    println!("y is {y}");
+    {
+        let y = y * 4;
+        println!("y is {y} in the inner scope");
+    }
+    println!("y is {y} in the outer scope");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+}
