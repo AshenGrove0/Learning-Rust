8773ecc995772f1be9d0d0e09d3a1cd388d2cfa1
AshenGrove0 <108214445+AshenGrove0@users.noreply.github.com>
2024-05-15 06:57:14 +0100

    Git push

---
+++ the-book/variables/target/debug/deps/variables-b82a665964a6d203.d
@@ -0,0 +1,5 @@
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/variables/target/debug/deps/variables-b82a665964a6d203: src/main.rs
+
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/variables/target/debug/deps/variables-b82a665964a6d203.d: src/main.rs
+
+src/main.rs:
---
+++ the-book/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/variables/target/debug/variables: /Users/tombreslin/Documents/GitHub/Learning-Rust/the-book/variables/src/main.rs
