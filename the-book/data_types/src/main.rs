a619ee14d2f8d3d57b3e15f358cacd5c3a262ee2
AshenGrove0 <108214445+AshenGrove0@users.noreply.github.com>
2024-05-16 07:26:01 +0100

    3.2 - Data types

--- the-book/data_types/src/main.rs
+++ the-book/data_types/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let guess: u32 = "42".parse().expect("NaN");
+    let hex = 0xfe;
+    let pi_3dp: f64 = 3.142;
+    let tup: (i32, i64, f32) = (200,300,4.21);
+    let (x,y,z) = tup;
+    println!("{x}");
+    let index_1 = tup.1;
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    let array: [i64;5] = [1,2,3,4,5];
+    let a = [3;5]; // [3,3,3,3,3]
+    let first = a[0];
+
 }
